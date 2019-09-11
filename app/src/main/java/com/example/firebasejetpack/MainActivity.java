3a4fccdcf37d6a47fa5ea2a114192e694a4bd1f8
Parag Dangriya <45040382+ParagsDangariya@users.noreply.github.com>
2019-09-11 16:58:44 -0400

    login fragment

--- app/src/main/java/com/example/firebasejetpack/MainActivity.java
+++ app/src/main/java/com/example/firebasejetpack/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.firebasejetpack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
